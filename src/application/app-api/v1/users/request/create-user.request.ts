d8a9e4d5567e769e7910342a684520ece3c30726
Jin <tkdtkdqja@naver.com>
2025-04-24 15:26:54 +0900

    Revert "test: 유저 목록 조회 기능 단위 테스트 코드 추가"
    This reverts commit fd4082b03dcca5b0d6ab9d7217eea226fdd7d1f0.

--- src/application/app-api/v1/users/request/create-user.request.ts
+++ src/application/app-api/v1/users/request/create-user.request.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserRequest {
-  @ApiProperty({ example: 'test' })
+  @ApiProperty({ required: true, example: 'test' })
   username: string;
 
-  @ApiProperty({ example: 'testPassword' })
+  @ApiProperty({ required: true, example: 'testPassword' })
   password: string;
 
   static toService(request: CreateUserRequest): any {
+    const { username, password } = request;
+
     return {
-      ...request,
+      username,
+      password,
     };
   }
 }
